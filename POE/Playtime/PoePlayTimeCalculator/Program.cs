cdf79b9625c4d31b77c5a11e8744479d73a6fec9
Maksim Scheierman <maksim.scheierman@gmx.de>
2023-07-28 18:54:27 +0200

    Successfully finished, but wrong amount of expierence in the JSON file

--- POE/Playtime/PoePlayTimeCalculator/Program.cs
+++ POE/Playtime/PoePlayTimeCalculator/Program.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.IO;
 using System.Text.Json;
+using System.Collections.Generic;
+
+
+
+
 
 public class CharacterData
 {
-    public string name { get; set; }
-    public string league { get; set; }
-    public int? classId { get; set; }
-    public int? ascendancyClass { get; set; }
-    public string @class { get; set; }
-    public int? level { get; set; }
-    public long? experience { get; set; }
-    public bool? pinnable { get; set; }
+    // public string name { get; set; }
+    // public string league { get; set; }
+    // public int classId { get; set; }
+    // public int ascendancyClass { get; set; }
+    // public string @class { get; set; }
+    // public int level { get; set; }
+    public long experience { get; set; }
+    //public bool pinnable { get; set; }
+
+    // Additional property to store the calculated playtime
+    public double overallPlaytime { get; set; }
+    public double overallXP { get; set; }
 }
 
 public class Program
@@ -22,11 +31,35 @@ public class Program
 
         try
         {
-            CharacterData data = ReadAndParseJson(jsonFilePath);
+            List<CharacterData> characterList = ReadAndParseJson(jsonFilePath);
 
-            // Now you can work with the 'data' object that holds the parsed JSON data.
-            Console.WriteLine($"Name: {data.name}");
-            // ... and so on.
+            double overallPlaytime = 0.0; // Declare the variable outside of the loop
+            double overallXP = 0.0;
+            int charCount=0;
+            double XP=0;
+
+            // Calculate playtime for each character and add it to the overallPlaytime property
+            foreach (CharacterData character in characterList)
+            {
+                charCount++;
+                XP += character.experience; 
+
+                if (character.experience >= 839850)
+                {
+                    overallXP += character.experience - 839850;
+                    overallPlaytime += 6.0; // Increment overallPlaytime by 6 hours
+                }
+            }
+
+            // Print the overall playtime 
+            double xpPerHour = 3000000;
+            double overallPlaytimeHours = overallXP / xpPerHour; // 2 million XP per hour assumed
+            overallPlaytime += overallPlaytimeHours;
+
+            //Console.WriteLine("Overall Playtime: " + overallPlaytime + " hours");
+            //Console.WriteLine("RestXP: " + overallXP + " XP");
+            Console.WriteLine("XP: " + XP);
+            Console.WriteLine("charCount: " + charCount);
         }
         catch (Exception ex)
         {
@@ -35,7 +68,7 @@ public class Program
         }
     }
 
-    static CharacterData ReadAndParseJson(string jsonFilePath)
+    static List<CharacterData> ReadAndParseJson(string jsonFilePath)
     {
         if (!File.Exists(jsonFilePath))
         {
@@ -46,10 +79,10 @@ public class Program
         {
             string jsonString = File.ReadAllText(jsonFilePath);
 
-            // Deserialize the JSON string to CharacterData object.
-            CharacterData data = JsonSerializer.Deserialize<CharacterData>(jsonString);
+            // Deserialize the JSON string to a list of CharacterData objects.
+            List<CharacterData> characterList = JsonSerializer.Deserialize<List<CharacterData>>(jsonString);
 
-            return data;
+            return characterList;
         }
         catch (Exception ex)
         {
